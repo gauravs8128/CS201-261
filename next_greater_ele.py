83b7587cca7b8c4ef0044e2c2dcf680fa07cc944
arpitpandey2 <147488848+arpitpandey2@users.noreply.github.com>
2024-09-04 16:43:48 +0530

    Add files via upload

---
+++ next_greater_ele.py
@@ -0,0 +1,58 @@
+'''def nextGreaterElements(A):
+    n = len(A)
+    result = [-1] * n  
+    answer = []  
+
+    for i in range(2 * n):
+        while answer and A[answer[-1]] < A[i % n]:
+            result[answer.pop()] = A[i % n]
+        if i < n:
+            answer.append(i)
+    
+    return result
+
+A = [1, 2, 1,9,6,7,3,4,5,-1]
+print(nextGreaterElements(A)) '''
+
+'''def nextGreaterElements(A):
+    n = len(A)
+    result = [-1] * n  
+    answer = []  
+    index = 0 
+    for i in range(2 * n):
+        
+        while index < len(answer) and A[answer[index]] < A[i % n]:
+            result[answer[index]] = A[i % n]
+            index += 1  
+        if i < n:
+            answer.append(i)
+    
+    return result
+
+A = [1, 2, 1, 9, 6, 7, 3, 4, 5, -1]
+print(nextGreaterElements(A))'''
+
+def nextGreaterElements(A):
+    n = len(A)
+    result = [-1] * n  
+    answer = []  # Stack to store indices
+    top = -1  # Top of the stack index
+
+    for i in range(2 * n):
+        while top >= 0 and A[answer[top]] < A[i % n]:
+            result[answer[top]] = A[i % n]
+            top -= 1  
+        
+        if i < n:
+            top += 1
+            if top < len(answer):
+                answer[top] = i  
+            else:
+                answer += [i]
+    
+    return result
+
+A = [1, 2, 1, 9, 6, 7, 3, 4, 5, -1]
+print(nextGreaterElements(A))
+
+
